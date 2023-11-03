03355164c41e54a426a9343c2ee1abf614d1d93e
foritesz <unghiadam74@gmail.com>
2023-11-03 14:12:43 +0100

    valahanyadik

--- Tikositas.py
+++ Tikositas.py
@@ -11,49 +11,75 @@ random.shuffle(key)
 
 #ENCRYPT
 plain_text = beleptetes.jelszo_bekerese()
-score=Snake.score
+score=4
 key = "your_encryption_key_here"  # Az itt lévő "your_encryption_key_here" helyére írd be a saját titkosítási kulcsodat
+processed_chars = set() # egy halmazt a már feldolgozott karakterek tárolására
+"""def atlakitas(plain_text):
+    cipher_text = ""
+    for char in plain_text:
+        if char.isalpha():
+            if char.islower():
+                cipher_text += "%" + char
+            else:
+                cipher_text += "!" + char
+        elif char.isdigit():
+            cipher_text += "=" + char
+        else:
+            cipher_text += char
+
+    return cipher_text"""
 
 def szam_titkositas(plain_text):
     cipher_text = ""
     for char in plain_text:
-        if char.isdigit():
+        if char.isdigit() and char not in processed_chars:
             index = int(char)
+            print(f"szám:{index}")
             if 0 <= index < len(key):
                 cipher_text += key[index]
+                processed_chars.add(key[index])  # Jelöld meg, hogy ezt a karaktert már feldolgoztad
             else:
                 cipher_text += char
         else:
             cipher_text += char
+
     return cipher_text
 
 def kisbetu_titkositas(plain_text):
     cipher_text = ""
     for char in plain_text:
-        if char.islower():
+        if char.islower() and char not in processed_chars:
             index = ord(char) - ord('a')
+            print(f"kisbetu:{index}")
             if 0 <= index < len(key):
                 cipher_text += key[index]
+                processed_chars.add(key[index])  # Jelöld meg, hogy ezt a karaktert már feldolgoztad
             else:
                 cipher_text += char
         else:
             cipher_text += char
+
     return cipher_text
 
 def nagybetu_titkositas(plain_text):
     cipher_text = ""
     for char in plain_text:
-        if char.isupper():
+        if char.isupper() and char not in processed_chars:
             index = ord(char) - ord('A')
+            print(f"nagybetu:{index}")
             if 0 <= index < len(key):
                 cipher_text += key[index]
+                processed_chars.add(key[index])  # Jelöld meg, hogy ezt a karaktert már feldolgoztad
             else:
                 cipher_text += char
         else:
             cipher_text += char
+
     return cipher_text
 
 def tikositas_pontszerint(score, plain_text):
+    """if score >= 1:
+        plain_text = atlakitas(plain_text)"""
     if score >= 2:
         plain_text = szam_titkositas(plain_text)
     if score >= 3:
@@ -65,6 +91,7 @@ def tikositas_pontszerint(score, plain_text):
 
 encrypted_text = tikositas_pontszerint(score, plain_text)
 felh=beleptetes.felhasznalonev()
+
 def fajl():
     with open('jelszo.txt', 'r') as f:
         lines = f.readlines()
@@ -79,19 +106,8 @@ def fajl():
 
 
 
-
 print(f"original message : {plain_text}")
 print(f"encrypted message: {encrypted_text}")
 
 fajl()
 
-#DECRYPT
-cipher_text = input("Enter a message to encrypt: ")
-plain_text = ""
-
-for letter in cipher_text:
-    index = key.index(letter)
-    plain_text += chars[index]
-
-print(f"encrypted message: {cipher_text}")
-print(f"original message : {plain_text}")
