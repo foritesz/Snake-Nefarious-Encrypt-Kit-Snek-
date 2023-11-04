3d007f498c0fae235f7360e6e9e3e8f6e696ecd9
foritesz <144954656+foritesz@users.noreply.github.com>
2023-11-04 22:59:05 +0100

    kezdünk oda érni

--- Tikositas.py
+++ Tikositas.py
@@ -3,7 +3,7 @@ import string
 import beleptetes
 import Snake
 
-chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = " " +string.digits + string.ascii_letters
 chars = list(chars)
 key = chars.copy()
 
@@ -12,8 +12,9 @@ random.shuffle(key)
 #ENCRYPT
 plain_text = beleptetes.jelszo_bekerese()
 score=4
-key = "your_encryption_key_here"  # Az itt lévő "your_encryption_key_here" helyére írd be a saját titkosítási kulcsodat
 processed_chars = set() # egy halmazt a már feldolgozott karakterek tárolására
+eredeti_chars=set()
+
 """def atlakitas(plain_text):
     cipher_text = ""
     for char in plain_text:
@@ -33,7 +34,8 @@ def szam_titkositas(plain_text):
     cipher_text = ""
     for char in plain_text:
         if char.isdigit() and char not in processed_chars:
-            index = int(char)
+            index = chars.index(char)
+            eredeti_chars.add(index)
             #original_char = plain_text[index]
             print(f"szám:{index}")
             if 0 <= index < len(key):
@@ -50,7 +52,8 @@ def kisbetu_titkositas(plain_text):
     cipher_text = ""
     for char in plain_text:
         if char.islower() and char not in processed_chars:
-            index = ord(char) - ord('a') #ord() függvény az adott karakter Unicode kódját adja vissza úgy hogy a kis abetütől kezdi ek nézni
+            index = chars.index(char) #ord() függvény az adott karakter Unicode kódját adja vissza úgy hogy a kis abetütől kezdi ek nézni
+            eredeti_chars.add(index)
             #original_char = plain_text[index]
             print(f"kisbetu:{index}")
             if 0 <= index < len(key):
@@ -67,7 +70,8 @@ def nagybetu_titkositas(plain_text):
     cipher_text = ""
     for char in plain_text:
         if char.isupper() and char not in processed_chars:
-            index = ord(char) - ord('A')
+            index = chars.index(char)
+            eredeti_chars.add(index)
             #original_char = plain_text[index]
             print(f"nagybetu:{index}")
             if 0 <= index < len(key):
@@ -102,7 +106,7 @@ def tikositas_pontszerint(score, plain_text):
 
     return keykod"""
 
-key2=unicode(plain_text)
+#key2=unicode(plain_text)
 encrypted_text = tikositas_pontszerint(score, plain_text)
 felh=beleptetes.felhasznalonev()
 
@@ -124,4 +128,16 @@ print(f"original message : {plain_text}")
 print(f"encrypted message: {encrypted_text}")
 
 fajl()
+def visszafejtes(cipher_text, key):
+    plain_text = ""
+    for letter in cipher_text:
+        index = key.index(letter)
+        plain_text += chars[index]
 
+    return plain_text
+
+decrypted_text = visszafejtes(encrypted_text, key)
+print(f"decrypted message: {decrypted_text}")
+
+print(processed_chars)
+print(eredeti_chars)
